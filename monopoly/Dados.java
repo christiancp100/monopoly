802daa0633ec47664ca32e976fa9391a6b07b4c6
María Caseiro Arias <maria@MacBook-Pro-de-Maria.local>
2018-10-25 20:21:27 +0200

    Cualquier cosa

--- monopoly/Dados.java
+++ monopoly/Dados.java
@@ -3,12 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package monopoly;
 
+import java.util.Random;
+
 /**
- *
- * @author christiancp
+ * 
+ * @author María Caseiro <mariacaseiro24@gmail.com>
  */
 public class Dados {
+
+    private int dado;
+    
+    //CONSTRUCTORES
+    public Dados(){
+        this.dado=2;
+    }
+    
+    //set dado no tiene sentido pq es un valor aleatorio en el que el usuario no toma ninguna decisión
+    
+    //GETTERS
+    public int getDado(){
+        return dado;
+    }
+    
+    //MÉTODOS
+    public int tirarDados(){
+        
+        int max=6;
+        int min=1;
+        
+        Random r=new Random();      
+        return r.nextInt(max-min+1)+min;
+    }
+    
+    public void comparar(Dados d2){
+        
+        if(this.dado==d2.getDado()){
+            System.out.println("Doble, lance otra vez.");
+        }
+    }
     
 }
