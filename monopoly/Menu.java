168024ed791089e28ba05e7e26edc08830358676
christiancp100 <christiancp100@gmail.com>
2018-11-10 11:20:14 +0100

    menu creada

--- monopoly/Menu.java
+++ monopoly/Menu.java
@@ -1,4 +1,21 @@
 package monopoly;
 
+import java.util.ArrayList;
+
 public class Menu {
+
+    private InterpreteComandos interprete;
+    private ArrayList<Avatar> avatares;
+    private Tablero tablero;
+
+    public Menu(InterpreteComandos interprete, Tablero tablero, ArrayList<Avatar> avatares){
+        this.interprete = interprete;
+        this.avatares = avatares;
+        this.tablero = tablero;
+    }
+
+    public void start(){
+
+
+    }
 }
